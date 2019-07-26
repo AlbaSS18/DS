8d5e07522988ab981b808305773f8b3a4f36e638
AlbaSS18 <albaserenasuarez1998@gmail.com>
2019-07-26 13:54:28 +0200

    Práctica 1 (Enunciado)

---
+++ Práctica1 (Enunciado)/src/parte2/Rental.java
@@ -0,0 +1,23 @@
+package parte2;
+
+public class Rental		// Alquiler 
+{
+	private Movie movie;
+	private int days;
+	
+	public Rental(Movie movie, int days) 
+	{
+		this.movie = movie;
+		this.days = days;
+	}
+
+	public int getDays() 
+	{
+		return days;
+	}
+
+	public Movie getMovie() 
+	{
+		return movie;
+	}
+}
